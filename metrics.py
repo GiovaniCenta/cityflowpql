2bffeac0f819c4a7f5b3521a58237564abcc6f7d
Giovani da Silva <geeowdk@gmail.com>
2022-11-17 17:53:12 -0300

    first commit

---
+++ metrics.py
@@ -0,0 +1,259 @@
+from msilib.schema import Class
+from queue import Empty
+from re import X
+import matplotlib.pyplot as plt
+import datetime
+import os
+import wandb
+from torch.utils.tensorboard import SummaryWriter
+
+class metrics():
+    def __init__(self, episodes, rewards1, rewards2):
+        self.episodes = episodes
+        self.rewards1 = rewards1
+        self.rewards2 = rewards2
+        self.paretor0 = []
+        self.paretor1 = []
+        self.nonDominatedPoints = []
+        self.ndPoints =[]
+        self.pdict = {}
+        self.xA0 = []
+        self.yA0 = []        
+        self.xA1 = []
+        self.yA1 = []
+        self.xA2 = []
+        self.yA2 = []
+        self.xA3 = []
+        self.yA3 = []
+        self.count = 0
+        self.path = ''
+        self.createLogDir()
+
+    def createLogDir(self):
+        e = datetime.datetime.now()
+        directory = e.strftime("%d#%m#%Y  %H-%M-%S")
+        self.path = os.path.join(os.getcwd() + '\\log','log '+ directory)
+        
+        if not os.path.exists(self.path):
+            os.mkdir(self.path)
+
+    def plotGraph(self):
+        
+        
+        fig, ax = plt.subplots()
+        ax.plot(self.episodes, self.rewards1)
+        ax.set_title('Treasure reward x Episodes')
+        #plt.show()
+        plt.savefig(self.path + '\\Treasure reward x Episodes')
+        
+        fig, ax2 = plt.subplots()
+        ax2.plot(self.episodes, self.rewards2)
+        ax2.set_title('Time penalty x Episodes')
+        
+        
+        plt.savefig(self.path + '\\Time penalty x Episodes')
+        plt.show()
+    
+    def plot_p_front(self,Xs,Ys,actionIndex,maxY = True,maxX = True):
+        
+        
+        Xs = self.paretor0
+        
+
+        Ys = self.paretor1
+        sorted_list = sorted([[Xs[i], Ys[i]] for i in range(len(Xs))], reverse=maxX)
+        pareto_front = [sorted_list[0]]
+        for pair in sorted_list[1:]:
+            if maxY:
+               
+                if pair[1] >= pareto_front[-1][1]:
+                    
+                    pareto_front.append(pair)
+            else:
+                if pair[1] <= pareto_front[-1][1]:
+                    pareto_front.append(pair)
+       
+        
+       
+       
+       
+        pf_X = []
+        pf_Y = []
+        best_y = [] 
+        best_x = []
+        for pair in pareto_front:
+            
+            
+            if pair[1] not in pf_Y:
+                best_y.append((pair[0],pair[1]))
+                pf_Y.append(pair[1])
+            else:
+                pf_Y.append(pair[1])
+
+            if pair[0] not in pf_X:
+                best_x.append((pair[0],pair[1]))
+                pf_X.append(pair[0])
+            else:
+                pf_X.append(pair[0])
+            
+        
+
+
+        frontier = []
+                
+        for p in best_y:
+            if p in best_x:
+                frontier.append(p)      
+            
+        pf_X = [pair[0] for pair in frontier]
+        pf_Y = [pair[1] for pair in frontier]    
+        plt.scatter(Xs,Ys)
+        plt.plot(pf_X, pf_Y)
+        plt.xlabel("Treasure Reward  " )
+        plt.ylabel("Time Penalty " )
+
+        plt.show()
+           
+        
+
+
+
+        
+    def plot_pareto_frontier(self):
+        '''Pareto frontier selection process'''
+        
+        #
+        self.plot_p_front(self.paretor0,self.paretor1,3)
+        
+        
+    def setup_wandb(self, project_name: str, experiment_name: str):
+        self.experiment_name = experiment_name
+        import wandb
+
+        wandb.init(
+            project=project_name,
+            sync_tensorboard=True,
+            config=self.get_config(),
+            name=self.experiment_name,
+            monitor_gym=True,
+            save_code=True,
+
+        )
+        self.writer = SummaryWriter(f"{self.experiment_name}")
+        # The default "step" of wandb is not the actual time step (gloabl_step) of the MDP
+        wandb.define_metric("*", step_metric="global_step")
+
+    def close_wandb(self):
+        import wandb
+        self.writer.close()
+        wandb.finish()
+
+    
+    def get_config(self) -> dict:
+        """Generates dictionary of the algorithm parameters configuration
+
+        Returns:
+            dict: Config
+        """
+
+    def plot_p_front2(self,Xs,Ys,actionIndex,maxY = True,maxX = True):
+        import numpy as np
+        """
+        sorted_list = sorted([[Xs[i], Ys[i],Zs[i]] for i in range(len(Xs))], reverse=maxX)
+        
+        pareto_front = [sorted_list[0]]
+        
+        
+        for pair in sorted_list[1:]:
+            #pareto_front.append(pair)
+            if maxY:
+               
+                if pair[1] >= pareto_front[-1][1]:
+                    
+                    pareto_front.append(pair)
+            else:
+                if pair[1] <= pareto_front[-1][1]:
+                    pareto_front.append(pair)
+     
+        
+        print(self.pdict)
+        """
+        frontier = []
+        
+        Xs = self.paretor0
+        Ys = self.paretor1
+        points = np.column_stack((Xs, Ys))
+        uniques = np.unique(points,axis=0)
+        
+        inputPoints = uniques.tolist()
+        paretoPoints, dominatedPoints = simple_cull(inputPoints, dominates)
+
+        print ("*"*8 + " non-dominated answers " + ("*"*8))
+        for p in paretoPoints:
+            frontier.append(p)
+            print (p)
+        print ("*"*8 + " dominated answers " + ("*"*8))
+        for p in dominatedPoints:
+            pass
+            #print (p)
+        #print(arr)
+
+        print(frontier)
+        
+  
+        pf_Xx = [pair[0] for pair in frontier]
+        pf_Yy = [pair[1] for pair in frontier]
+          
+
+        
+      
+        
+        
+        Xs = points[:,0]
+        Ys = points[:,1]
+        
+
+        
+        plt.plot(Ys,Xs)
+        plt.scatter(pf_Xx, pf_Yy)
+        plt.xlabel("Treasure Reward  " )
+        plt.ylabel("Time Penalty " )
+        
+
+        
+        
+        
+        
+               
+        plt.show()
+def simple_cull(inputPoints, dominates): 
+    paretoPoints = set()
+    candidateRowNr = 0
+    dominatedPoints = set()
+    while True:
+        candidateRow = inputPoints[candidateRowNr]
+        inputPoints.remove(candidateRow)
+        rowNr = 0
+        nonDominated = True
+        while len(inputPoints) != 0 and rowNr < len(inputPoints):
+            row = inputPoints[rowNr]
+            if dominates(candidateRow, row):
+                # If it is worse on all features remove the row from the array
+                inputPoints.remove(row)
+                dominatedPoints.add(tuple(row))
+            elif dominates(row, candidateRow):
+                nonDominated = False
+                dominatedPoints.add(tuple(candidateRow))
+                rowNr += 1
+            else:
+                rowNr += 1
+
+        if nonDominated:
+            # add the non-dominated point to the Pareto frontier
+            paretoPoints.add(tuple(candidateRow))
+
+        if len(inputPoints) == 0:
+            break
+    return paretoPoints, dominatedPoints
+def dominates(row, candidateRow):
+    return sum([row[x] >= candidateRow[x] for x in range(len(row))]) == len(row) 
---
+++ wandb/run-20221115_222834-lci3n285/files/code/deepseatreasure-pareto/agent.py
@@ -0,0 +1,337 @@
+from multiprocessing.resource_sharer import stop
+from pathlib import Path
+from xml.etree.ElementTree import tostring
+
+import copy
+import gym
+import numpy as np
+from scipy import rand
+#from sympy import Q
+import pygame
+from gym.spaces import Box, Discrete
+from pygmo import hypervolume
+from metrics import metrics as met
+import deepst
+metrics = met([],[],[])
+import wandb
+
+
+class Pareto():
+    def __init__(self, env,actionsMethods, choose_action, ref_point, nO=2,nS = 64, gamma=1.):
+        self.env = env
+        self.actionsMethods=actionsMethods
+        self.choose_action = choose_action
+        self.gamma = gamma
+
+        self.ref_point = ref_point
+
+        self.nS = nS
+        
+        self.nA = env.action_space.n
+        env.nA = self.nA
+        self.non_dominated = [[[np.zeros(nO)] for _ in range(self.nA)] for _ in range(self.nS)]
+        self.avg_r = np.zeros((self.nS, self.nA, nO))
+        self.n_visits = np.zeros((self.nS, self.nA))
+        self.epsilon = 1
+        self.epsilonDecrease = 0.99
+        self.stateList = []
+
+        
+        self.polIndex = 0
+        self.log = False
+        self.qtable = np.zeros((self.nS, self.nA, nO),dtype=object)
+
+        
+
+    def initializeState(self):
+        return self.flatten_observation(self.env.reset())
+
+    def flatten_observation(self, obs):
+        #print(obs[1])
+        
+        if type(obs[1]) is dict:
+        	return int(np.ravel_multi_index((0,0), (11, 11)))
+        #print(type(obs[1]))
+           
+        else:
+            return int(np.ravel_multi_index(obs, (11, 11)))
+
+
+    def train(self,max_episodes,max_steps,log):
+        
+        self.polDict = np.zeros((max_episodes,self.nS, self.nA, 2))
+        self.log = log
+        if log:
+            metrics.setup_wandb("pql", "pql")
+
+        numberOfEpisodes = 0
+        episodeSteps = 0
+
+        #line 1 -> initialize q_set
+        print("-> Training started <-")
+        #line 2 -> for each episode
+        while numberOfEpisodes  < max_episodes:
+
+            acumulatedRewards = [0,0]
+            episodeSteps = 0
+            terminal = False
+            #line 3 -> initialize state s
+            s = self.initializeState()
+            #print(s)
+            
+            
+            #line 4 and 11 -> repeat until s is terminal:
+            while terminal is not True and episodeSteps < max_steps:
+                #env.render()
+                s,terminal,reward = self.step(s)
+                #print(s, episodeSteps)
+                episodeSteps += 1
+                acumulatedRewards[0] += reward[0]
+                acumulatedRewards[1] += reward[1]
+
+
+            metrics.rewards1.append(acumulatedRewards[0])
+            metrics.rewards2.append(acumulatedRewards[1])
+            metrics.episodes.append(numberOfEpisodes)
+            log_dict = {
+                    "reward0":acumulatedRewards[0],
+                    "reward1":acumulatedRewards[1],
+                    "episode":numberOfEpisodes
+
+
+
+                }
+            if log:
+                wandb.log(log_dict)
+            
+            
+            #print()
+            #self.qtable (estados,ações,objetivos)
+            
+            self.episodeqtable = copy.deepcopy(self.qtable)
+            self.polDict[numberOfEpisodes] = self.episodeqtable
+            self.polIndex +=1
+            
+            metrics.pdict = self.polDict
+            numberOfEpisodes+=1
+
+
+    ######################### PARETO TRAINING ################
+
+        print(self.polDict.shape)
+        
+        self.currentepisode = 0
+        q_set = self.polDict
+        numberOfEpisodes = 0
+        episodeSteps = 0
+
+        #line 1 -> initialize q_set
+        print("-> Pareto Training started <-")
+
+        #line 2 -> for each episode
+        
+        while self.currentepisode  < max_episodes:
+            print("Pareto ep:" + str(self.currentepisode))
+            ParetoacumulatedRewards = [0,0,0]
+            episodeSteps = 0
+            
+
+
+            #line 3 -> initialize state s
+            s = self.initializeState()
+            
+            terminalpareto = False
+            
+            #line 4 and 11 -> repeat until s is terminal:
+            while terminalpareto is not True and episodeSteps < max_steps:
+                s,terminalpareto,reward = self.stepPareto(s)
+                #print(s, episodeSteps)
+                episodeSteps += 1
+                ParetoacumulatedRewards[0] += reward[0]
+                ParetoacumulatedRewards[1] += reward[1]
+                
+            
+            log_dict2 = {
+                    "paretoreward0":ParetoacumulatedRewards[0],
+                    "paretoreward1":ParetoacumulatedRewards[1],
+                    "paretoepisode":self.currentepisode
+
+
+
+                }
+            if log:
+                wandb.log(log_dict2)
+
+            metrics.paretor0.append(ParetoacumulatedRewards[0])
+            metrics.paretor1.append(ParetoacumulatedRewards[1])
+
+
+
+            self.currentepisode+=1
+
+        if log:
+            data = [[x, y] for (x, y) in zip(metrics.paretor0, metrics.paretor1)]
+            table = wandb.Table(data=data, columns = ["x", "y"])
+            wandb.log({"my_custom_plot_id" : wandb.plot.scatter(table, "x", "y", title="Custom Y vs X Scatter Plot")})
+
+
+            metrics.close_wandb()
+
+
+    def stepPareto(self,s):
+        
+        self.actionsMethods.epsilon = 0 
+        
+        
+                
+            
+                
+        q_set = self.polDict[self.currentepisode][s]
+        
+        q_set = q_set[:, np.newaxis,: ]
+                
+        action = self.choose_action(s, q_set)
+        next_state, reward, terminal, _ = self.env.step(action)
+        next_s = ''.join(str(next_state))
+        if next_s not in self.stateList:
+            self.stateList.append(next_s)
+        next_s = self.stateList.index(next_s)
+        nd = self.update_non_dominated(s, action, next_s)
+        self.n_visits[s, action] += 1
+        self.avg_r[s, action] += (reward - self.avg_r[s, action]) / self.n_visits[s, action] 
+        return next_s,terminal,reward
+        
+        
+
+
+
+
+
+
+
+
+
+
+        
+
+
+    def step(self,s):
+        
+        
+        
+
+        #line 5 -> Choose action a from s using a policy derived from the Qˆset’s
+        
+        q_set = self.compute_q_set(s)
+        action = self.choose_action(s, q_set)
+        
+        #metrics for pareto plot
+        self.qcopy = copy.deepcopy(q_set)
+
+
+
+        #line 6 ->Take action a and observe state s0 ∈ S and reward vector r ∈ R
+        next_state, reward, terminal, _ = self.env.step(action)
+        next_s = self.flatten_observation(next_state)
+        
+        
+        
+        
+        nd = self.update_non_dominated(s, action, next_s)
+        
+        
+        #line 9 -> Update avg immediate reward
+        self.n_visits[s, action] += 1
+
+        self.avg_r[s, action] += (reward - self.avg_r[s, action]) / self.n_visits[s, action]
+
+        self.actionsMethods.epsilon *= self.actionsMethods.epsilonDecrease
+        
+
+        return next_s,terminal,reward
+
+    
+    def compute_q_set(self, s):
+        q_set = []
+        for a in range(self.env.nA):
+            nd_sa = self.non_dominated[s][a]
+            rew = self.avg_r[s, a]
+            
+          
+            q_set.append([rew + self.gamma*nd for nd in nd_sa])
+        return np.array(q_set)
+
+
+    def update_non_dominated(self, s, a, next_state):
+        q_set_n = self.compute_q_set(next_state)
+        # update for all actions, flatten
+        solutions = np.concatenate(q_set_n, axis=0)
+
+        # compute pareto front
+        self.non_dominated[s][a] = self.actionsMethods.get_non_dominated(solutions)
+        return self.non_dominated[s][a]
+
+class actionMethods():
+    def __init__(self,epsilon,epsilonDecrease):
+        self.epsilon = epsilon
+        self.epsilonDecrease = epsilonDecrease
+
+    
+
+    def get_action(self,s, q,env):
+        q_values = self.compute_hypervolume(q, q.shape[0], ref_point)
+
+        if np.random.rand() >= self.epsilon:
+            
+            return np.random.choice(np.argwhere(q_values == np.amax(q_values)).flatten())
+        else:
+            
+            return env.action_space.sample()
+
+    def compute_hypervolume(self,q_set, nA, ref):
+        q_values = np.zeros(nA)
+        for i in range(nA):
+            # pygmo uses hv minimization,
+            # negate rewards to get costs
+            points = np.array(q_set[i]) * -1.
+            hv = hypervolume(points)
+            # use negative ref-point for minimization
+            q_values[i] = hv.compute(ref*-1)
+        return q_values
+
+
+    def get_non_dominated(self,solutions):
+        is_efficient = np.ones(solutions.shape[0], dtype=bool)
+        
+        for i, c in enumerate(solutions):
+            if is_efficient[i]:
+                # Remove dominated points, will also remove itself
+                is_efficient[is_efficient] = np.any(solutions[is_efficient] > c, axis=1)
+                # keep this solution as non-dominated
+                is_efficient[i] = 1
+
+        return solutions[is_efficient]
+
+if __name__ == '__main__':
+    #envinronment variables
+    import gym
+    from gym import wrappers
+    from deepst import DeepSeaTreasure
+    env = DeepSeaTreasure()
+    numberOfStates = 121
+    numberOfObjectives = 2
+    epsilon = 1
+    epsilonDecrease = 0.999
+    acMeth = actionMethods(epsilon,epsilonDecrease)
+    ref_point = np.array([0, -25])
+    gamma = 0.9
+
+    #agent call
+    agent = Pareto(env,acMeth, lambda s, q: acMeth.get_action(s, q, env), ref_point, nO=numberOfObjectives,nS = numberOfStates, gamma=gamma)
+    agent.train(50,400,log = True)
+
+    #metrics
+    #metrics.plotGraph()
+    metrics.plot_pareto_frontier()
+
+    print("-> Done <-")
